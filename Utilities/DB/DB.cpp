4e3c5b3f2480b3ac8fef14058184cd184b9ef6b0
현수 김 <hun4159@naver.com>
2020-04-23 02:32:05 +0900

    200423 02:31

---
+++ Utilities/DB/DB.cpp
@@ -0,0 +1,161 @@
+#include "DB.h"
+
+Utilities::MY_SQL::cDB_Result::cDB_Result()
+{
+	mCurrent_Row = -1;
+	mSql_Result = nullptr;
+}
+
+Utilities::MY_SQL::cDB_Result::~cDB_Result()
+{
+	if (mSql_Result != nullptr)
+	{
+		//결과값 저장한 변수 초기화
+		mysql_free_result(mSql_Result);
+	}
+}
+
+bool Utilities::MY_SQL::cDB_Result::Move_Row(unsigned int _index)
+{
+	mysql_data_seek(mSql_Result, _index - 1);
+	if (mysql_fetch_row(mSql_Result) == NULL)
+	{
+		return false;
+	}
+	mCurrent_Row = _index;
+	return true;
+}
+
+bool Utilities::MY_SQL::cDB_Result::Next_Row()
+{
+	if (mysql_fetch_row(mSql_Result) == NULL)
+	{
+		return false;
+	}
+	mCurrent_Row++;
+	return true;
+}
+
+void Utilities::MY_SQL::cDB_Result::Set_Result(MYSQL_RES* _result)
+{
+	mCurrent_Row = 0;
+	mSql_Result = _result;
+}
+
+int Utilities::MY_SQL::cDB_Result::Current_Row_Index()
+{
+	return mCurrent_Row;
+}
+
+const char* Utilities::MY_SQL::cDB_Result::Current_Row(int _index)
+{
+	//index 범위 검사
+	if (mSql_Result == nullptr || mSql_Result->field_count <= _index) return nullptr;
+
+	return mSql_Result->current_row[_index];
+}
+
+const char* Utilities::MY_SQL::cDB_Result::operator[](unsigned int _index)
+{
+	//index 범위 검사
+	if (mSql_Result == nullptr || mSql_Result->field_count <= _index) return nullptr;
+
+	return mSql_Result->current_row[_index];
+}
+
+const char* Utilities::MY_SQL::cDB_Result::Current_Row(const char* _filed)
+{
+	//열 갯수 만큼 검사
+	for (int i = 0; i < (mSql_Result->field_count); i++)
+	{
+		//일치시 해당 필드값 반환
+		if (!strcmp(_filed, mSql_Result->fields[i].name))
+		{
+			return mSql_Result->current_row[i];
+		}
+	}
+	return nullptr;
+}
+
+int Utilities::MY_SQL::cDB_Result::Row_Count()
+{
+	if (mSql_Result == nullptr) return 0;
+	return mSql_Result->row_count;
+}
+
+int Utilities::MY_SQL::cDB_Result::Column_Count()
+{
+	if (mSql_Result == nullptr) return 0;
+	return mSql_Result->field_count;
+}
+
+const char* Utilities::MY_SQL::cDB_Result::Flied_Name(unsigned int _index)
+{
+	if (mSql_Result == nullptr || mSql_Result->field_count <= _index) return nullptr;
+	return mSql_Result->fields[_index].name;
+}
+
+
+//-----------------------------------------------------------------------------------------------------
+
+
+Utilities::MY_SQL::cDatabase::cDatabase()
+{
+	mConnection = mysql_init(NULL);
+}
+
+Utilities::MY_SQL::cDatabase::~cDatabase()
+{
+	if (mConnection)
+	{
+		MY_SQL::cDatabase::Close();
+	}
+}
+
+bool Utilities::MY_SQL::cDatabase::Conncetion(const char* _HOST, const char* _USER, const char* _PASS, const char* _NAME, unsigned int _PORT, const char* _UNIX_SOCK, unsigned long _CLIENT_FLAG)
+{
+	//DB접속 
+	if (mysql_real_connect(mConnection, _HOST, _USER, _PASS, _NAME, _PORT, (char*)NULL, 0) == NULL)
+	{
+		//실패시 에러 출력
+		fprintf_s(stderr, "Mysql connection error : %s \n", mysql_error(mConnection));
+		return false;
+	}
+
+	printf_s("Mysql connection Success DB NAME : %s \n", _NAME);
+	return true;
+}
+
+bool Utilities::MY_SQL::cDatabase::Run_SQL(const char* _query)
+{
+	//쿼리 적용
+	int retval = mysql_query(mConnection, _query);
+	if (retval != 0)
+	{
+		//실패시 에러 출력
+		fprintf(stderr, "Mysql query error : %s", mysql_error(mConnection));
+		return false;
+	}
+
+	//쿼리 적용 결과값 저장
+
+	return true;
+}
+
+bool Utilities::MY_SQL::cDatabase::Get_Result(OUT cDB_Result& _result)
+{
+	MYSQL_RES* result = nullptr;
+	//결과값을 저장한다.
+	if ((result = mysql_store_result(mConnection)) != NULL)
+	{
+		_result.Set_Result(result);
+		return true;
+	}
+	return false;
+}
+
+void Utilities::MY_SQL::cDatabase::Close()
+{
+	//DB 연결해제
+	mysql_close(mConnection);
+}
